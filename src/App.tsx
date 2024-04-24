bcce17f23284b5960deb4de8f39ba9ddfcd24b8e
Arielparicio28 <arielaparicio100@gmail.com>
2024-04-24 09:02:12 +0200

    Creación de entorno de desarrollo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className = " text-center  border-violet-600 text-red-800 m-4 text-3xl ">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
