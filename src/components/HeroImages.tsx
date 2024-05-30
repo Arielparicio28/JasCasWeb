fb846a8063db5d88a9ed5f596e71003018b8dc62
Arielparicio28 <arielaparicio100@gmail.com>
2024-05-30 21:09:52 +0200

    Change image_hero

--- src/components/HeroImages.tsx
+++ src/components/HeroImages.tsx
@@ -4,7 +4,7 @@ const HeroImages =() => {
   return (
 <div>
 <h1 className='text-right mr-4 mt-4 text-2xl font-font text-white '> Jas Cas</h1> 
-<img src="fondo_pantalla.jpg" alt="mujer kuna" className='w-full h-full '/>
+<img src="fondo_recortado.jpg" alt="mujer kuna" className='w-full h-full '/>
 </div>
   )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,13 +6,13 @@ const NavBar = () => {
       <ul className='flex justify-center text-xl space-x-8 list-none m-2 opacity-80'>
         <li className='flex items-center transition ease-in-out delay-150 hover:bg-neutral-300 p-2 rounded-lg hover:scale-110 duration-300 font-font'>
           <Link to="/" className='flex items-center space-x-2'>
-            <img src="eco-home.png" alt="icono_home" className='w-16 h-16'/>
+            <img src="eco-home.png" alt="icono_home" className='w-10 h-10 lg:w-16 lg:h-16'/>
             <span className='text-black '>Home</span>
           </Link>
         </li>
         <li className='flex items-center transition ease-in-out delay-150 hover:bg-neutral-300 p-2 rounded-lg hover:scale-110 duration-300 font-font'>
           <Link to="/images" className='flex items-center space-x-2'>
-            <img src="paisaje.png" alt="icono_imagen" className='w-16 h-16'/>
+            <img src="paisaje.png" alt="icono_imagen" className='w-10 h-10 lg:w-16 lg:h-16'/>
             <span className='text-black '>Images</span>
           </Link>
         </li>
