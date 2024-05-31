cb84336bd9f41c9b22fece9f12c25492a93e6446
Arielparicio28 <arielaparicio100@gmail.com>
2024-05-31 09:08:05 +0200

    Cambiando la ruta de las imagenes a /

--- src/components/HeroImages.tsx
+++ src/components/HeroImages.tsx
@@ -3,7 +3,7 @@ import './hero.css';
 const HeroImages = () => {
   return (
     <div className="relative">
-      <div className="lg:bg-[url('./public/fondo_pantalla.jpg')] bg-[url('./public/recortada.jpg')] bg-cover bg-center w-full h-screen">
+      <div className="lg:bg-[url('/fondo_pantalla.jpg')] bg-[url('/recortada.jpg')] bg-cover bg-center w-full h-screen">
         <h1 className='text-right mr-4 mt-4 text-3xl font-font text-red-600'>Jas Cas</h1>
       </div>
     </div>
