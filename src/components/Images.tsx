fef415e127350b65e806c4d2070ac58def4d7ccf
Arielparicio28 <arielaparicio100@gmail.com>
2024-11-12 13:14:40 +0100

    Responsive imagenes

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -9,7 +9,7 @@ const Images = () => {
         <img
           src="tota.JPG"
           alt="Ilustración de la artista con su hermano y tía "
-          className="w-80 h-80 border-2 border-gray-600 hover:animate-zoomOut"
+          className="w-60 h-60 border-2 border-gray-600 hover:animate-zoomOut lg:w-90 lg:h-60"
         />
       </Link>
 
@@ -17,7 +17,7 @@ const Images = () => {
       <img
         src="shell.png"
         alt="Fondo marino"
-        className="w-80 h-80 border-2 border-purple-600 hover:animate-zoomOut"
+        className="w-60 h-60 border-2 border-purple-600 hover:animate-zoomOut  lg:w-90 lg:h-60"
       />
       </Link>
 
@@ -26,7 +26,7 @@ const Images = () => {
       <img
         src="sea.png"
         alt="fondo marino"
-        className="w-80 h-80 border-2 border-purple-600 hover:animate-zoomOut"
+        className="w-60 h-60 border-2 border-purple-600 hover:animate-zoomOut  lg:w-90 lg:h-60"
       />
       </Link>
 
@@ -35,7 +35,7 @@ const Images = () => {
       <img
         src="WhiteBird.jpg"
         alt="pareja de gatos"
-        className="w-80 h-80 border-2 border-purple-600 hover:animate-zoomOut"
+        className="w-60 h-60 border-2 border-purple-600 hover:animate-zoomOut  lg:w-90 lg:h-60"
       />
       </Link>
 
@@ -44,7 +44,7 @@ const Images = () => {
       <img
         src="piñas.jpg"
         alt="piñas"
-        className="w-80 h-80 border-2 border-purple-600 hover:animate-zoomOut"
+        className="w-60 h-60 border-2 border-purple-600 hover:animate-zoomOut  lg:w-90 lg:h-60"
       />
       </Link>
 
@@ -52,7 +52,7 @@ const Images = () => {
       <img
         src="african_woman.jpg"
         alt="african woman"
-        className="w-80 h-80 border-2 border-purple-600 hover:animate-zoomOut"
+        className="w-60 h-60 border-2 border-purple-600 hover:animate-zoomOut  lg:w-90 lg:h-60"
       />
       </Link>
 
@@ -61,7 +61,7 @@ const Images = () => {
       <img
         src="Basic_Survival.jpg"
         alt="Basic Survival"
-        className="w-80 h-80 border-2 border-purple-600 hover:animate-zoomOut"
+        className="w-60 h-60 border-2 border-purple-600 hover:animate-zoomOut  lg:w-90 lg:h-60"
       />
       </Link>
 
@@ -70,7 +70,7 @@ const Images = () => {
       <img
         src="Boon_page.jpg"
         alt="figura de cuento"
-        className="w-80 h-80 border-2 border-purple-600 hover:animate-zoomOut"
+        className="w-60 h-60 border-2 border-purple-600 hover:animate-zoomOut  lg:w-90 lg:h-60"
       />
       </Link>
 
@@ -79,7 +79,7 @@ const Images = () => {
       <img
         src="Boon_page2.jpg"
         alt="figura de cuento"
-        className="w-80 h-80 border-2 border-purple-600 hover:animate-zoomOut"
+        className="w-60 h-60 border-2 border-purple-600 hover:animate-zoomOut  lg:w-90 lg:h-60"
       />
       </Link>
 
@@ -88,7 +88,7 @@ const Images = () => {
       <img
         src="cantante.jpg"
         alt="cantante"
-        className="w-80 h-80 border-2 border-purple-600 hover:animate-zoomOut"
+        className="w-60 h-60 border-2 border-purple-600 hover:animate-zoomOut  lg:w-90 lg:h-60"
       />
       </Link>
 
@@ -98,7 +98,7 @@ const Images = () => {
 <img
   src="cantante_1.jpg"
   alt="cantante"
-  className="w-80 h-80 border-2 border-purple-600 hover:animate-zoomOut"
+  className="w-60 h-60 border-2 border-purple-600 hover:animate-zoomOut  lg:w-90 lg:h-60"
 />
 </Link>
 
@@ -109,7 +109,7 @@ const Images = () => {
 <img
   src="cantante_2.jpg"
   alt="cantante"
-  className="w-80 h-80 border-2 border-purple-600 hover:animate-zoomOut"
+  className="w-60 h-60 border-2 border-purple-600 hover:animate-zoomOut lg:w-90 lg:h-60"
 />
 </Link>
 
@@ -120,7 +120,7 @@ const Images = () => {
 <img
   src="card.jpg"
   alt="card"
-  className="w-80 h-80 border-2 border-purple-600 hover:animate-zoomOut"
+  className="w-60 h-60 border-2 border-purple-600 hover:animate-zoomOut  lg:w-90 lg:h-60"
 />
 </Link>
 
