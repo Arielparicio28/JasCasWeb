6947ef2e90f01ec9dbda482ac0d679d5893d6fe0
Arielparicio28 <arielaparicio100@gmail.com>
2024-11-25 20:59:22 +0100

    Quitando la navegacion hasta q se haga el copyright de ls imagenes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
  import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Images from './pages/Images'; 
 import Home from './pages/Home';
-import NavBar from './components/NavBar';
 import ImagesView from './components/ImagesView';
 import About from './pages/About';
 import Children from './pages/Children';
 import Slider3D from './components/Slider3D';
 
-
+//Solo importar el navbar cuando ponga la app a funcionar
 const App = () => {
  
 
   return (
     <>
     <Router>
-      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/images" element={<Images />} />
